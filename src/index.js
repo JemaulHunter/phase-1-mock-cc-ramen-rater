b295b4b7c486d1f731bfa49acf5aa106e6004061
Jemaul Hunter <jemaulhunter@gmail.com>
2022-07-26 13:49:58 -0400

    Mock upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-// write your code here
+/* Setup
+Run json-server --watch db.json to get the backend 
+started
+Open the index.html file on your browser
+Write your code in the index.js file
+*/
+//Done
+/*
+Endpoints
+Your base URL for your API will 
+be: http://localhost:3000
+The endpoints you may need are:
+GET /ramens
+GET /ramens/:id
+*/
+
+/*core deliverables
+see all ramen images in the div with the id 
+of ramen-menu.
+display the image of each ramen using an img 
+tag inside the #ramen-menu div
+*/
+// const img = new Image();
+// var div = document.getElementById('ramen-menu')
+// img.onload = function() {
+//     div.innerHTML += <img src = "'+imgsrc+'"/>>
+// }
+// img.src = 'image.jpg'
+var element = document.createElement("#ramen-menu");
+image.src = "#ramen-menu";
+var block = document.getElementById("ramens");
+block.appendChild(img);
+/*
+2. click on an image from the #ramen-menu div 
+and see all the info displayed inside the #ramen-detail 
+div where it says insert comment here and insert rating 
+here.
+*/
+/*
+3. Create a new ramen after submitting the new-ramen 
+form. The new ramen should be added to 
+the #ramen-menu div.
+*/
